3be37e0228ccd96ffa91ac9c4107080e48ab1ec4
Mohamed Vakkayil <mohamedvakkayil@live.in>
2021-12-11 12:18:20 +0530

    templates cleared

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
 	path('',views.regs,name=''),
-	path('spouse',views.further,name='spouse'),
-	path('child',views.studs,name='child'),
-	path('thanks', views.thanks, name='success'),
-	path('pay', views.pay, name='pay'),
+	path('spouse/',views.further,name='spouse'),
+	path('child/',views.studs,name='child'),
+	path('thanks/', views.thanks, name='success'),
+	path('pay/', views.pay, name='pay'),
 
 ]
